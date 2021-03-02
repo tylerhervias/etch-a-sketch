4590327b1d4d8ad3483689da157b0a7fd37b2560
tylerhervias <tylerhervias37@gmail.com>
2021-03-02 07:22:53 -0500

    Add functionality to change grid size

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let gridContainer = document.querySelector('.grid-container');
+let gridContainer = document.querySelector(".grid-container");
+let button = document.querySelector(".button");
+
+button.addEventListener("click", resetGrid);
 
 // Creates a grid with a specific number of rows and columns
 function setGrid(rows, cols) {
@@ -9,13 +12,29 @@ function setGrid(rows, cols) {
   for (i = 0; i < (rows * cols); i++) {
     let gridElement = document.createElement("div");
     gridContainer.appendChild(gridElement);
-    gridElement.classList = 'grid-element';
+    gridElement.classList = "grid-element";
     gridElement.addEventListener("mouseover", setColor);
   };
 };
 
+// Resets each grid square to its default state
+function resetGrid() {
+  // Prompts the user for a new grid size
+  let newSize = prompt("Enter new grid size");
+
+  // Removes each individual square from the grid
+  let gridArray = Array.from(gridContainer.childNodes);
+  gridArray.forEach((element) => {
+    gridContainer.removeChild(element);
+  });
+
+  // Creates the new grid size
+  setGrid(newSize, newSize);
+}
+
+// Changes the background color of each square on hover
 function setColor(event) {
-  event.target.style.backgroundColor = '#000000';
+  event.target.style.backgroundColor = "#000000";
 }
 
 setGrid(16, 16);
